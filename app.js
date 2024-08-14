9ba43f4632ae286902467aefb7cf46cbfc1e4dd8
BryandMarti <164084388+BryandMarti@users.noreply.github.com>
2024-08-14 14:37:24 -0400

    Solving Deployment issues
    There is an issue with the deployment portion of the code, had to bring the server folder to the front directory

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //middleware directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, './lrnr/dist')));
 
 //anthropic api key connection and setting up the quizcall function
 const anthropic = new Anthropic();
