95006c3e634b0b091b75b0c431d02fccc4a9b4f1
cperalt <145234956+cperalt@users.noreply.github.com>
2024-08-15 00:55:38 -0400

    Configured Cor to give access to deployed site

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 //connection to .env file and setting up the environment variables
 dotenv.config();
 app.use(cors());
+app.use(cors({
+  origin: 'https://lrnr-quiz-frontend-fjv4uf46m-carlos-projects-4fb13629.vercel.app/'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //middleware directory
