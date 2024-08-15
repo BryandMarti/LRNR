01ccece4155d67136da91293f26927037b11a2fa
cperalt <145234956+cperalt@users.noreply.github.com>
2024-08-15 00:41:27 -0400

    Fixed Navbar links to use React Navlink

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="navbar-fixed">
       <nav className="blue">
         <div className="nav-wrapper container">
-          <a href="/" className="brand-logo">lrnr</a>
+          <NavLink to="/" className="brand-logo">lrnr</NavLink>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/account">Account</a></li>
-            <li><a href="/quiz-generator">Quiz Generator</a></li>
+            <li><NavLink to="/account">Account</NavLink></li>
+            <li><NavLink  to="/quiz-generator">Quiz Generator</NavLink></li>
           </ul>
         </div>
       </nav>
