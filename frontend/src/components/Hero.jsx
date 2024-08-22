2dfe6ec421b62e38ee419c4f4980c00dae881826
BryandMarti <164084388+BryandMarti@users.noreply.github.com>
2024-08-22 14:22:02 -0400

    Links Fixed
    Changed a Tags to Navlinks to make it work in deployment

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <section className="section center-align">
       <img src={Logo} alt="lrnr logo" className="responsive-img" style={{ maxWidth: '27%', marginTop: '3.5rem' }} />
       <h5>Your guided path to programming enlightenment</h5>
-      <a href="/quiz-generator" className="btn-large teal" style={{marginTop: '2.5rem'}}>Begin Journey</a>
+      <NavLink to="/quiz-generator" className="btn-large teal" style={{marginTop: '2.5rem'}}>Begin Journey</NavLink>
     </section>
   );
 };
