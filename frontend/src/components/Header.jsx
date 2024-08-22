8db05e4b279c5c8f91063600bdb7f9e798601064
jemima139 <165708488+jemima139@users.noreply.github.com>
2024-08-22 12:15:44 -0400

    Updated CSS and Home Components
    Created hamburger menu, fixed footer positions, and fixed styling of the UI across home and account pages.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="navbar-fixed">
       <nav className="blue">
         <div className="nav-wrapper container">
           <NavLink to="/" className="brand-logo">lrnr</NavLink>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <a href="#" className="sidenav-trigger right" onClick={toggleMenu}>
+            <i className="material-icons">menu</i>
+          </a>
+          <ul id="nav-mobile" className={`right hide-on-med-and-down ${isOpen ? 'show-menu' : ''}`}>
             <li><NavLink to="/account">Account</NavLink></li>
-            <li><NavLink  to="/quiz-generator">Quiz Generator</NavLink></li>
+            <li><NavLink to="/quiz-generator">Quiz Generator</NavLink></li>
           </ul>
+          {isOpen && (
+            <ul className="sidenav">
+              <li><NavLink to="/" onClick={toggleMenu}>Home</NavLink></li>
+              <li><NavLink to="/account" onClick={toggleMenu}>Account</NavLink></li>
+              <li><NavLink to="/quiz-generator" onClick={toggleMenu}>Quiz Generator</NavLink></li>
+            </ul>
+          )}
         </div>
       </nav>
     </header>
