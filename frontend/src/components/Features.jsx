3900a191f25888b8025ce91f75a1661ad257c11a
BryandMarti <164084388+BryandMarti@users.noreply.github.com>
2024-08-22 13:41:10 -0400

    Tab Icon, Tab Name
    Personalized the Tab icon and name to fit our website's purpose

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -6,21 +6,21 @@ const Features = () => {
       <div className="row">
         <div className="col s12 m4">
           <div className="icon-block">
-            <h2 style={{ color: '#1ee9b6', fontSize: '48px' , textAlign: 'center' }} ><i className="material-icons" style={{fontSize: '3.5rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>flash_on</i></h2>
+            <h2 style={{ color: '#1ee9b6', fontSize: '48px' , textAlign: 'center' }} ><i className="material-icons" style={{fontSize: '2.5rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>flash_on</i></h2>
             <h5 className="center" style={{color: 'black'}}>Personalized Quizzes</h5>
             <p className="light" style={{padding: '15px', color: 'black'}}>Greetings, young padawan. Are you ready to embark on a journey of personalized enlightenment through the art of coding? Our app, can create custom quizzes that align with your coding skills and interests. Whether you are a novice or a master, our system can generate questions that will test your proficiency in programming languages, tools, and concepts </p>
           </div>
         </div>
         <div className="col s12 m4">
           <div className="icon-block">
-            <h2 style={{ color: '#1ee9b6', fontSize: '50px' , textAlign: 'center' }} ><i className="material-icons" style={{display: 'flex', height: '5vh'}}>attach_payments</i></h2>
+            <h2 style={{ color: '#1ee9b6', fontSize: '50px' , textAlign: 'center' }} ><i className="material-icons" style={{display: 'flex', height: '3.5vh'}}>attach_payments</i></h2>
             <h5 className="center" style={{color: 'black'}}>Rewarding</h5>
             <p className="light" style={{padding: '15px', color: 'black'}}>Our app is designed to be both challenging and rewarding, so you can learn new concepts while enjoying the process. With our personalized quiz app, you can track your progress, compete with your peers, and discover new areas of expertise. The journey of a thousand lines of code begins with a single keystroke</p>
           </div>
         </div>
         <div className="col s12 m4">
           <div className="icon-block" >
-            <h2 style={{ color: '#1ee9b6', fontSize: '48px' , textAlign: 'center' }} ><i className="material-icons" style={{fontSize: '3.5rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>person</i>
+            <h2 style={{ color: '#1ee9b6', fontSize: '48px' , textAlign: 'center' }} ><i className="material-icons" style={{fontSize: '2.5rem', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>person</i>
             </h2>
             <h5 className="center" style={{color: 'black'}}>Personal SME</h5>
             <p className="light" style={{padding: '15px', color: 'black'}}>Welcome to the path of knowledge. Our app is like having a personal subject matter expert at your side, guiding you on your journey towards wisdom</p>
